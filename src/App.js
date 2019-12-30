eea333bf2c0adf6dd3e30b1b471820807b5e4e8f
unknown <mejia.alan.r@gmail.com>
2019-12-30 13:26:11 -0300

    Create the base project files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div >
+      <HomePage title="HATS" subtitle="SHOP NOW"/>
+      <HomePage title="DRESS" subtitle="SHOP NOW"/>
+      <HomePage title="COATS" subtitle="SHOP NOW"/>
+      <HomePage title="COATS" subtitle="SHOP NOW"/>
+      <HomePage title="COATS" subtitle="SHOP NOW"/>
+    </div>
+  );
+}
+
+export default App;
